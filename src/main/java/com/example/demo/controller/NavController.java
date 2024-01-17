2f2b4673a447e74715e7c119f3eb99485b9e6ee4
raghavendrareddy2001 <raghavajev@gmail.com>
2024-01-17 12:52:45 +0530

    1st version of tune hub

---
+++ src/main/java/com/example/demo/controller/NavController.java
@@ -0,0 +1,28 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@Controller
+public class NavController {
+	@GetMapping("/login")
+	public String login() {
+		//TODO: process POST request
+
+		return "login";
+	}
+	@GetMapping("/registration")
+	public String registration() {
+		//TODO: process POST request
+
+		return "registration";
+	}
+	@GetMapping("/newSong")
+	public String newSong() {
+		return "newSong";
+	}
+	
+
+}
